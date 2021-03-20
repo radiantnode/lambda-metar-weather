33fa51137761eba0b6161395fb6715d0e79b315b
Michael Simmons <michael@radiantnode.com>
2021-03-20 18:23:50 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# METAR weather data lambda function
+
+This function returns METAR weather data for a given airport using the [Get METAR Report endpoint](https://avwx.docs.apiary.io/#reference/0/metar/get-metar-report) from the [Aviation Weather REST API](https://avwx.rest/).
---
+++ index.js
@@ -0,0 +1,27 @@
+const request = require('request');
+
+module.exports.metarWeather = (event, _context, callback) => {
+  const params = event.queryStringParameters;
+
+  const respond = (statusCode, body) => {
+    callback(null, {
+      statusCode: statusCode,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json'
+      },
+      body: typeof(body) == 'object' ? JSON.stringify(body) : body
+    });
+  }
+
+  if(params && params.icao) {
+    request({
+      url: `https://avwx.rest/api/metar/${params.icao}`,
+      method: event.httpMethod,
+      headers: { 'Authorization': `Token ${process.env.AVWX_TOKEN}` },
+    }, (_err, _response, body) => respond(200, body));
+  } else {
+    respond(401, { error: "You must provide an ICAO in your query." });
+  }
+
+};
